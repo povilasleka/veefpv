69d97a15a89fd0a5665e27bd4f514b4cb6c6717e
Povilas L. <povilasleka@gmail.com>
2026-07-11 01:53:20 +0300

    fix: add media prefix migration

--- src/payload/migrations/index.ts
+++ src/payload/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260710_212504_initial_schema from './20260710_212504_initial_schema';
 import * as migration_20260710_215014_add_hero_global from './20260710_215014_add_hero_global';
+import * as migration_20260710_225103_add_media_prefix_field from './20260710_225103_add_media_prefix_field';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260710_215014_add_hero_global.up,
     down: migration_20260710_215014_add_hero_global.down,
-    name: '20260710_215014_add_hero_global'
+    name: '20260710_215014_add_hero_global',
+  },
+  {
+    up: migration_20260710_225103_add_media_prefix_field.up,
+    down: migration_20260710_225103_add_media_prefix_field.down,
+    name: '20260710_225103_add_media_prefix_field'
   },
 ];
